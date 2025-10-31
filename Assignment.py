ebd13d6ab8e6e6435d2117b55f85f26e8fc14cde
subratapanda24 <pandasubrata024@example.com>
2025-10-31 23:26:49 +0530

    First commit

---
+++ Assignment.py
@@ -0,0 +1,13 @@
+# Exercise 1: Mailing Address
+
+print("Subrata Panda")
+print("Flat No. 111,Kasturba Nagar")
+print("Vashinaka, Chembur")
+print("Mumbai, Maharashtra - 400074")
+print("India")
+
+#Exercise 2 : Hello
+# Exercise 2: Helloo
+
+name = input("Enter your name: ")
+print("Hello, " + name + "!")
