0e944d5f25ddf631143e71acc23e562cf394bb7b
subratapanda24 <pandasubrata024@example.com>
2025-10-31 23:54:15 +0530

    Added Assignment

---
+++ Assignment2.py
@@ -0,0 +1,22 @@
+# Until You Type Stop : Bot
+
+while True:
+    question = input("You: ")
+    
+    if question.lower() == "stop":
+        print("Bot: Bye")
+        break
+    else:
+        print("Bot: I hear you!\n")
+
+
+#Assignment Reminder System
+
+while True:
+    alert = input("Have you submitted your assignment? ")
+    
+    if alert.lower() == "done":
+        print("Bot: Great! You are all done!")
+        break
+    else:
+        print("Bot: Still waiting... Please submit it!\n")
